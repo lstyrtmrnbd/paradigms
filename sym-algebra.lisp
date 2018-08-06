7002b014d195606ad9a950881dd7783d2d795d2b
Marisa Kirisame <soymilk@airmail.cc>
2018-08-05 20:03:20 -0400

    simplification rules

--- sym-algebra.lisp
+++ sym-algebra.lisp
@@ -2,12 +2,18 @@
 ;;; Includes the necessary code frome the previous chapter,
 ;;; but pat-match and rule-based-translator live in "pat-match.lisp"
 
+(defstruct (rule (:type list)) pattern response)
+
+(defun rule-pattern (rule) (first rule))
+(defun rule-response (rule) (second rule))
+
 (defstruct (exp (:type list)
                 (:constructor mkexp (lhs op rhs)))
   op lhs rhs)
 
 (defun exp-p (x) (consp x))
 (defun exp-args (x) (rest x))
+
 
 (defun binary-exp-p (x)
   (and (exp-p x) (= (length (exp-args x)) 2)))
@@ -20,6 +26,15 @@
                   (list (exp-lhs exp) (exp-op exp) (exp-rhs exp))
                   exp))))
 
+
+;; redefinition of function in "pat-match.lisp"
+(defun variable-p (exp)
+  "Variables are only the symbols M thru Z."
+  (member exp '(x y z m n o p q r s t u v w))) ; x y z first to match quicker
+
+(pat-match-abbrev 'x+ '(?+ x))
+(pat-match-abbrev 'y+ '(?+ y))
+
 (defparameter *infix->prefix-rules*
   (mapcar #'expand-pat-match-abbrev
           '(((x+ = y+) (= x y))
@@ -31,9 +46,6 @@
             ((x+ / y+) (/ x y))
             ((x+ ^ y+) (^ x y))))
   "A list of rules, ordered by precedence.")
-
-(defun rule-pattern (rule) (first rule))
-(defun rule-response (rule) (second rule))
 
 (defun infix->prefix (exp)
   "Translate an infix expression into prefix notation."
@@ -53,3 +65,36 @@
          (list (first exp) (infix->prefix (rest exp))))
         (t (error "Illegal exp"))))
 
+(defparameter *simplification-rules*
+  (mapcar #'infix->prefix
+          '((x + 0 = x)
+            (0 + x = x)
+            (x + x = 2 * x)
+            (x - 0 = x)
+            (0 - x = - x)
+            (x - x = 0)
+            (- - x = x)
+            (x * 1 = x)
+            (1 * x = x)
+            (x * 0 = 0)
+            (0 * x = 0)
+            (x * x = x ^ 2)
+            (x / 0 = undefined)
+            (0 / x = 0)
+            (x / 1 = x)
+            (x / x = 1)
+            (0 ^ 0 = undefined)
+            (x ^ 0 = 1)
+            (0 ^ x = 0)
+            (1 ^ x = 1)
+            (x ^ 1 = x)
+            (x ^ -1 = 1 / x)
+            (x * (y / x) = y)
+            ((y / x) * x = y)
+            ((y * x) / x = y)
+            ((x * y) / x = y)
+            (x + - x = 0)
+            ((- x) + x = 0)
+            (x + y - x = y))))
+
+(defun ^ (x y) "Exponentiation" (expt x y))
